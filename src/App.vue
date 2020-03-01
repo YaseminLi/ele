4d3ea78c6953ab952b3404899f2f6a7b4dc3e4cf
YaseminLi <yaseminli@163.com>
2020-03-01 17:34:22 +0800

    without cube-ui

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"/>
+    <div class="tab border-1px">
+      <router-link tag="div" class="tab-item" to="/goods">商品</router-link>
+      <router-link tag="div" class="tab-item" to="/ratings">评价</router-link>
+      <router-link tag="div" class="tab-item" to="/seller">商家</router-link>
+    </div>
+    <div class="content">
+      <keep-alive>
+        <router-view :seller="seller"></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import header from "components/header/header";
+export default {
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.get();
+  },
+  methods: {
+    get: function() {
+      this.$http.get("/api/seller").then(
+        response => {
+          this.seller = response.body;
+        },
+        () => {
+          console.log("获取商家数据失败");
+        }
+      );
+    }
+  },
+  components: {
+    "v-header": header
+  }
+};
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus' >
+@import './common/stylus/mixin.styl'
+@import '../../common/stylus/variable.styl'
+#app
+  .tab
+    display: flex
+    align-items: center
+    height: 40px
+    width: 100%
+    border-1px($color-row-line)
+    .tab-item
+      flex: 1
+      font-size: 14px
+      color: $color-grey-s
+      text-align: center
+    .router-link-active
+      color: $color-red
+</style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const appData = require('./data.json');
+const seller = appData.seller;
+const goods = appData.goods;
+const ratings = appData.ratings;
+function resolve (dir) {
+	return path.join(__dirname, dir)
+};
+module.exports = {
+	chainWebpack: (config)=>{
+            config.resolve.alias
+						.set('components', resolve('src/components'))
+						.set('common', resolve('src/common'))
+	},
+	devServer: {
+		before: function(app, server) {
+		  app.get('/api/seller', function(req, res) {
+			res.json(seller);
+		  });
+		  app.get('/api/goods', function(req, res) {
+			res.json(goods);
+		  });
+		  app.get('/api/ratings', function(req, res) {
+			res.json(ratings);
+		  });
+		}
+	  }
+};
